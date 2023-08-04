b327700202783a74f48911c26dcf69df284b6840
Ubuntu <comp141@ML-RefVm-949980.zv0ixxhdtw1epaxq4zps5kvxsc.ex.internal.cloudapp.net>
2023-08-04 02:50:37 +0000

    2nd commit. Editing main.sh

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # comp141-extra-credit
+
+This is a repo to practice writting a shell script with multiple commands.
--- main.sh
+++ main.sh
@@ -1,1 +1,32 @@
-hello
+#!/bin/bash
+
+mkdir output
+
+cp testfile1.txt output/ #copy testfile into /output
+
+cd output/
+
+cat testfile1.txt > read.txt #read testfile1 and save that output to read.txt
+
+pwd > pwd.txt
+
+ls > ls.txt
+
+alias date="today's date (according to vm) is: '+%m/%d/%y'" | date > date.txt
+
+wc -w testfile1.txt > textcount.txt	#word count .txt file, send output to textcont
+
+ps | head -n 5 > process.txt		#run ps command, read first words of output up to 5,
+						#then send that txt to process.txt
+ifconfig | head -n 5 > netstat.txt
+mount | head -n 5 > mount.txt
+
+echo "this file is empty on purpose, but it should have global rwx permissions" > permissions.txt 
+chmod 777 permissions.txt #give +rwx permissions to all groups
+
+TESTENV1="test"
+
+grep -E '^[A-Za-z]{3,}$' testfile1.txt > regex.txt #all words w/ 3+ letters
+
+
+cd .. #cd back to extra-credit
